cddda1bfd57990e767a7a2546c06f37f6134364a
Victor Lee <myleevictor@gmail.com>
2018-03-04 22:18:20 +0800

    added additional passing tests for filterNum

--- test/filterNum.test.js
+++ test/filterNum.test.js
@@ -1,8 +1,15 @@
 var filterNum = require("../src/filterNum.js");
 
 describe('filter Number from array', () => {
-    
-});
-test("filterNum([1,2,'a','b']) returns [1,2]", () => {
-    expect(filterNum([1,2,'a','b'])).toEqual([1,2]);
+    test("filterNum([1,2,'a','b']) returns [1,2]", () => {
+      expect(filterNum([1, 2, "a", "b"])).toEqual([1, 2]);
+    });
+
+    test("filterNum([1,'a','b',0,15]) returns [1,0,15]", () => {
+      expect(filterNum([1, "a", "b", 0, 15])).toEqual([1, 0, 15]);
+    });
+
+    test("filterNum([1, 2,'aasf','1',123,'123']) returns [1,2,123]", () => {
+      expect(filterNum([1, 2, "aasf",'1',123,'123'])).toEqual([1,2,123]);
+    });
 });
